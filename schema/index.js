af95e8e94f0a76e294e5ecaef9db4274f236f58b
Akshay Kadam <akshaykadam0904@gmail.com>
2017-05-06 01:00:32 +0530

    :art: Complete Project :checkered_flag:

--- schema/index.js
+++ schema/index.js
@@ -3,34 +3,42 @@ import {
 	GraphQLObjectType,
 	GraphQLString,
 	GraphQLInt,
+	GraphQLList,
 	GraphQLSchema
 } from 'graphql'
 
 
 const CompanyType = new GraphQLObjectType({
 	name: 'CompanyQueryType',
-	fields: {
-		'id': {type: GraphQLString},
-		'name': {type: GraphQLString},
-		'description': {type: GraphQLString}
-	}
+	fields: () => ({
+			'id': {type: GraphQLString},
+			'name': {type: GraphQLString},
+			'description': {type: GraphQLString},
+			'user': {
+				type: new GraphQLList(UserType),
+				resolve(parentValue,args) {
+					return axios.get(`http://localhost:3000/users`)
+						.then(res => res.data)
+				}
+			}
+		})
 })
 
 const UserType = new GraphQLObjectType({
 	name: 'UserQueryType',
-	fields: {
-		'id': {type: GraphQLString},
-		'name': {type: GraphQLString},
-		'gender': {type: GraphQLString},
-		'age': {type: GraphQLInt},
-		'company': {
-			type: CompanyType,
-			resolve(parentValue,args) {
-				return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
-					.then(res => res.data)
+	fields: () => ({
+			'id': {type: GraphQLString},
+			'name': {type: GraphQLString},
+			'gender': {type: GraphQLString},
+			'age': {type: GraphQLInt},
+			'company': {
+				type: CompanyType,
+				resolve(parentValue,args) {
+					return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+						.then(res => res.data)
+				}
 			}
-		}
-	}
+		})
 })
 
 const RootQuery = new GraphQLObjectType({
@@ -41,6 +49,14 @@ const RootQuery = new GraphQLObjectType({
 			args: {'id': {type: GraphQLString}},
 			resolve(parentValue,args) {
 				return axios.get(`http://localhost:3000/users/${args.id}`)
+					.then(res => res.data)
+			}
+		},
+		company: {
+			type: CompanyType,
+			args: {'id': {type: GraphQLString}},
+			resolve(parentValue,args) {
+				return axios.get(`http://localhost:3000/companies/${args.id}`)
 					.then(res => res.data)
 			}
 		}
